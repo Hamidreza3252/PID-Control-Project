eb4e831f96de7d01ef9961474665cbb41b959fba
Hamidreza3252 <Hamid.Mirkhani@gmail.com>
2020-03-25 07:54:36 -0500

    ready to submit

--- src/PID.cpp
+++ src/PID.cpp
@@ -4,27 +4,66 @@
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
 
-PID::PID() {}
-
-PID::~PID() {}
-
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+// --------------------------------------------------------------------------------------------------------------------
+PID::PID()
+{
+  i_steer_error_ = 0.0;
+  i_throtle_error_ = 0.0;
 }
 
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+// --------------------------------------------------------------------------------------------------------------------
+PID::~PID()
+{
 }
 
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+// --------------------------------------------------------------------------------------------------------------------
+void PID::Init(double kp_steer, double ki_steer, double kd_steer, double kp_throtle, double ki_throtle, double kd_throtle)
+{
+  kp_steer_ = kp_steer;
+  ki_steer_ = ki_steer;
+  kd_steer_ = kd_steer;
+
+  i_steer_error_ = 0.0;
+
+  kp_throtle_ = kp_throtle;
+  ki_throtle_ = ki_throtle;
+  kd_throtle_ = kd_throtle;
+
+  i_throtle_error_ = 0.0;
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+void PID::UpdateSteerError(double cte, double delta_t)
+{
+  p_steer_error_ = cte;
+  d_steer_error_ = (cte - prev_steer_cte_) / delta_t;
+  i_steer_error_ += cte;
+
+  prev_steer_cte_ = cte;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+double PID::TotalSteerError()
+{
+  return -(kp_steer_ * p_steer_error_ + kd_steer_ * d_steer_error_ + ki_steer_ * i_steer_error_);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+void PID::UpdateThrotleError(double speed, double target_speed, double delta_t)
+{
+  double cte = speed - target_speed;
+
+  p_throtle_error_ = cte;
+  d_throtle_error_ = (cte - prev_throtle_cte_) / delta_t;
+  i_throtle_error_ += cte;
+
+  prev_throtle_cte_ = cte;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+double PID::TotaThrotleError()
+{
+  return -(kp_throtle_ * p_throtle_error_ + kd_throtle_ * d_throtle_error_ + ki_throtle_ * i_throtle_error_);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
--- src/PID.h
+++ src/PID.h
@@ -1,8 +1,9 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
- public:
+class PID
+{
+public:
   /**
    * Constructor
    */
@@ -17,34 +18,50 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
+  void Init(double kp_steer, double ki_steer, double kd_steer, double kp_throtle, double ki_throtle, double kd_throtle);
 
   /**
    * Update the PID error variables given cross track error.
    * @param cte The current cross track error
    */
-  void UpdateError(double cte);
+  void UpdateSteerError(double cte, double delta_t);
 
   /**
    * Calculate the total PID error.
    * @output The total PID error
    */
-  double TotalError();
+  double TotalSteerError();
 
- private:
+  void UpdateThrotleError(double speed, double target_speed, double delta_t);
+  double TotaThrotleError();
+
+private:
   /**
    * PID Errors
    */
-  double p_error;
-  double i_error;
-  double d_error;
+  double p_steer_error_;
+  double i_steer_error_;
+  double d_steer_error_;
+
+  double prev_steer_cte_;
 
   /**
    * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+   */
+  double kp_steer_;
+  double ki_steer_;
+  double kd_steer_;
+
+  double p_throtle_error_;
+  double i_throtle_error_;
+  double d_throtle_error_;
+
+  // double speed_;
+  double kp_throtle_;
+  double ki_throtle_;
+  double kd_throtle_;
+
+  double prev_throtle_cte_;
 };
 
-#endif  // PID_H
+#endif // PID_H
